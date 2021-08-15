818c510898cbc1a2257e9142b68d720bcdd43ec4
William Chau <wchau@oraculo.nl>
2021-08-15 09:19:59 +0000

    -refactored -add comments

--- src/app/mag-activeren.service.ts
+++ src/app/mag-activeren.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +8,16 @@ export class MagActiverenService implements CanActivate {
 
   constructor() { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (!this.validateZipcode(route.params["id"])) {
       console.log('Ongeldig argument'); return false;
     }
-    // if (isNaN(route.params["id"]) == true) {
-    //   console.log("Ongeldig argument " + route.params["id"]); return false;
-    // }
     return true;
   }
 
-  validateZipcode(zipcode:string) {
-    var regex = /^[1-9][0-9]{3}[\s]?[A-Za-z]{2}$/i;
+  validateZipcode(zipcode: string) {
+    //postcode moet 4 cijfers en 2 letters zijn
+    var regex = /^[0-9]{4}[\s]?[A-Za-z]{2}$/i;
     return regex.test(zipcode);
   }
 }
